14cf047464487e4e89d79e6a6918fc15adefcb12
Lance Ren <lanceren@yunify.com>
2019-05-06 18:46:31 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Prnyself/modHello
+
+go 1.12
---
+++ hello.go
@@ -0,0 +1,10 @@
+package modHello
+
+import "fmt"
+
+func Hi(name string) string {
+    if name == "" {
+        return "Hello world"
+    }
+    return fmt.Sprintf("Hello %s", name)
+}
