4df6a3bdc64d420076fa94e5451bd1d848dbe796
Lance Ren <lanceren@yunify.com>
2019-05-07 15:41:29 +0800

    modify for git rebase test

--- hello.go
+++ hello.go
@@ -3,8 +3,8 @@ package modHello
 import "fmt"
 
 func Hi(name string) string {
-    if name == "" {
-        return "Hello world"
-    }
-    return fmt.Sprintf("Hello %s", name)
+	if name == "" {
+		return "Hello world!"
+	}
+	return fmt.Sprintf("Hello %s", name)
 }
